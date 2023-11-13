187d71164f11b8e8b12f2a58943c1236a557f8f1
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2023-11-13 15:46:30 +0100

    add ex1 test using random data set

--- src/test/kotlin/exercise1/TPMMSExerciseTests.kt
+++ src/test/kotlin/exercise1/TPMMSExerciseTests.kt
@@ -1,12 +1,16 @@
 package exercise1
 
 import de.hpi.dbs2.dbms.*
+import de.hpi.dbs2.dbms.utils.RelationUtils
+import de.hpi.dbs2.dbms.utils.RelationUtils.fill
 import de.hpi.dbs2.dbms.utils.RelationUtils.loadCSV
 import de.hpi.dbs2.exercise1.SortOperation
 import de.hpi.dbs2.exerciseframework.getChosenImplementation
+import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
 
 class TPMMSExerciseTests {
     private fun getImplementation(blockManager: BlockManager, sortColumnIndex: Int): SortOperation =
@@ -105,14 +109,89 @@ class TPMMSExerciseTests {
                     TPMMSExerciseTests::class.java.getResourceAsStream("sorted_by_col2.output.csv")!!
                 )
             }
-            assertEquals(controlRelation.joinToString(), outputRelation.joinToString())
+            assertTrue(
+                RelationUtils.equalContent(blockManager, controlRelation, outputRelation),
+                "tuple values deviate"
+            )
 
             assertEquals(3 * 6, cost.ioCost)
         }
     }
 
     @Test
-    fun `TPMMS returns error when relation is too large to sort`() {
+    fun `TPMMS sorts random data correctly`() {
+        val columnDefinition = ColumnDefinition(
+            ColumnDefinition.ColumnType.DOUBLE,
+        )
+
+        with(
+            DBMS(
+                totalBlocks = 25,
+                blockCapacity = 10
+            )
+        ) {
+            val inputRelation = createRelation(blockManager, columnDefinition)
+            val outputRelation = createRelation(blockManager, columnDefinition)
+            val controlRelation = createRelation(blockManager, columnDefinition)
+            val comparator = columnDefinition.getColumnComparator(0)
+
+            val random = Random(0)
+            val count = 5858
+            val column = List(count) {
+                columnDefinition.createTuple()
+                    .apply { set(0, random.nextDouble()) }
+            }
+            inputRelation.fill(blockManager) { filler ->
+                column.forEach { filler.add(it) }
+            }
+            controlRelation.fill(blockManager) { filler ->
+                column.sortedWith(comparator).forEach { filler.add(it) }
+            }
+
+            fun Relation.countTuples(): Int = iterator().asSequence().sumOf { block ->
+                blockManager.load(block).use {
+                    block.size
+                }
+            }
+
+            assertEquals(count, inputRelation.countTuples())
+
+            val sortOperation = getImplementation(blockManager, 0)
+
+            val estimateIOCostCost = trackIOCost {
+                assertEquals(0, blockManager.usedBlocks)
+                val estimatedCost = sortOperation.estimatedIOCost(inputRelation)
+                assertEquals(0, blockManager.usedBlocks)
+                assert(estimatedCost == inputRelation.estimatedBlockCount()*3 || estimatedCost == inputRelation.estimatedBlockCount()*4)
+            }
+            assertEquals(0, estimateIOCostCost.ioCost)
+
+            val sortCost = trackIOCost {
+                assertEquals(0, blockManager.usedBlocks)
+                sortOperation.execute(inputRelation, outputRelation)
+                assertEquals(0, blockManager.usedBlocks)
+            }
+
+            assertEquals(
+                controlRelation.iterator().asSequence().count(),
+                outputRelation.iterator().asSequence().count(),
+                "block count deviates"
+            )
+
+            assertEquals(count, outputRelation.countTuples())
+
+            assertTrue(
+                RelationUtils.equalContent(blockManager, controlRelation, outputRelation),
+                "tuple values deviate"
+            )
+
+            assertEquals(2 * inputRelation.estimatedBlockCount(), sortCost.inputCost)
+            assertEquals(1 * inputRelation.estimatedBlockCount(), sortCost.outputCost)
+        }
+    }
+
+    @Test
+    fun `TPMMS throws error when relation is too large to sort`() {
         val columnDefinition = ColumnDefinition(
             ColumnDefinition.ColumnType.INTEGER,
         )
